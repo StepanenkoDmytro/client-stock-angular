b703f2e88df5a5553bee1bfd926889f19d2527e9
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2023-10-23 14:39:34 +0300

    amout to expends

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import * as moment from 'moment';
 import { MNY_WIDGET } from 'src/app/domain/default-widget-state.domain';
 import { IBudgetExpense } from 'src/app/domain/widget.domain';
@@ -9,28 +9,29 @@ import { ExpendBudgetService } from 'src/app/service/expend-budget.service';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PortfolioComponent {
   public componentURL = MNY_WIDGET;
   public expenseArr: IBudgetExpense[] = [];
+  //ngrx
 
   constructor(
     private dialog: DialogService,
-    private expendBudget: ExpendBudgetService) {
-    this.expendBudget.load(moment()).subscribe(task => {
-      this.expenseArr = task;
-    }, err => console.log(err));
-  }
+    private expendBudget: ExpendBudgetService
+  ) { }
 
   public openBudgetTracker() {
     this.dialog.openBudgetTracker().afterClosed().subscribe(result => {
-
-      if (result) {
-        this.expendBudget.load(moment()).subscribe(task => {
-          this.expenseArr = task;
-        }, err => console.log(err));
+      if (!result) {
+        return;
       }
+
+      this.expendBudget.load(moment()).subscribe(task => {
+        this.expenseArr = task;
+      }, err => console.log(err));
+
     });
   }
 }
