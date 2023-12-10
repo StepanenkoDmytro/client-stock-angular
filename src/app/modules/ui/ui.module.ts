4bde05e0ecd808e4943deea2ae7072dfaa915c53
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2023-12-10 21:56:14 +0200

    add to the ui module custom button-toggle & refactor styles for stock-card

--- src/app/modules/ui/ui.module.ts
+++ src/app/modules/ui/ui.module.ts
@@ -5,29 +5,18 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { MaterialModule } from './material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DonatChartComponent } from './components/donat-chart/donat-chart.component';
-
+import { ButtonToggleComponent } from './components/button-toggle/button-toggle.component';
 
 export const COMPONENTS = [
   ResizableComponent,
   NotFoundComponent,
   DonatChartComponent,
+  ButtonToggleComponent,
 ];
 
 @NgModule({
-  declarations: [
-    ...COMPONENTS,
-  ],
-  imports: [
-    CommonModule,
-    MaterialModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
-  exports: [
-    ...COMPONENTS,
-    MaterialModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+  declarations: [...COMPONENTS],
+  imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
+  exports: [...COMPONENTS, MaterialModule, FormsModule, ReactiveFormsModule],
 })
-export class UiModule { }
+export class UiModule {}
