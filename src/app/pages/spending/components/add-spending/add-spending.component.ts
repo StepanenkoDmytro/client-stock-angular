b7eef12e2c9d025b27f5c553a4619dac160e5050
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2023-12-22 16:50:24 +0200

    add-spending: input name of product & input amount with buttons layout

--- src/app/pages/spending/components/add-spending/add-spending.component.ts
+++ src/app/pages/spending/components/add-spending/add-spending.component.ts
@@ -3,22 +3,21 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 
 const UI_MODULES = [
   MatSelectModule,
   MatFormFieldModule,
-  MatIconModule
+  MatIconModule,
+  MatButtonModule,
+  MatInputModule
 ];
-
-const MATERIAL_MODULES = [
-  MatButtonModule
-]
 
 @Component({
   selector: 'pgz-add-spending',
   standalone: true,
-  imports: [...UI_MODULES, ...MATERIAL_MODULES],
+  imports: [...UI_MODULES],
   templateUrl: './add-spending.component.html',
   styleUrl: './add-spending.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
