8121da61530c5d25c52bae61ca5f1b6aa490befe
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2023-12-28 16:03:27 +0200

    test connect to server api

--- src/app/pages/savings/savings.component.ts
+++ src/app/pages/savings/savings.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { TotalInfoComponent } from './components/total-info/total-info.component';
 import { ButtonToggleComponent } from '../../core/UI/components/button-toggle/button-toggle.component';
 import {MatIconModule} from '@angular/material/icon';
@@ -8,6 +8,8 @@ import { StockStatisticComponent } from './components/stock-saving-wrapper/stock
 import { ACCOUNT_STOCKS_MOCK } from '../../domain/mock.domain';
 import { IPortfolioStock } from '../../domain/savings.domain';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { CoinService } from './service/coin.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 const UI_COMPONENTS = [
@@ -24,17 +26,52 @@ const MATERIAL_MODULES = [
   MatFormFieldModule
 ];
 
+enum AssetsType {
+  COIN,
+  STOCK,
+}
+
+interface IAssets {
+  assetType: AssetsType,
+  symbol: string,
+  name: string,
+  count: number,
+  avgPrice: number,
+  created?: Date,
+  updated?: Date
+};
+
 @Component({
   selector: 'pgz-savings',
   standalone: true,
-  imports: [...UI_COMPONENTS, ...MATERIAL_MODULES],
+  imports: [...UI_COMPONENTS, ...MATERIAL_MODULES, HttpClientModule],
   templateUrl: './savings.component.html',
   styleUrl: './savings.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [CoinService],
 })
-export class SavingsComponent {
+export class SavingsComponent implements OnInit {
   public stocks: IPortfolioStock[] = ACCOUNT_STOCKS_MOCK;
   public isPortfolioFrame: boolean = true;
+
+  constructor(private coinService: CoinService) {}
+  
+  public ngOnInit(): void {
+  //  this.coinService.test().subscribe((response) => {
+    
+  //   const resp: IAssets = {
+  //     assetType: AssetsType.COIN,
+  //     symbol: response.symbol,
+  //     name: response.name,
+  //     count: 0,
+  //     avgPrice: response.price,
+  //   };
+  //   console.log('Success', response);
+  // },
+  // (error) => {
+  //   console.error('Error', error);
+  // });
+  }
 
   public onChangeFrame(frame: boolean): void {
     this.isPortfolioFrame = frame;
