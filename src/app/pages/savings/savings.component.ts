259adb551f432b029d65a1dc626c35f5b23bb4dd
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2024-01-05 13:59:43 +0200

    add chips filters

--- src/app/pages/savings/savings.component.ts
+++ src/app/pages/savings/savings.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { TotalInfoComponent } from './components/total-info/total-info.component';
 import { ButtonToggleComponent } from '../../core/UI/components/button-toggle/button-toggle.component';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTabsModule} from '@angular/material/tabs';
-import { IAsset, IPortfolioStock } from '../../domain/savings.domain';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { IAsset } from '../../domain/savings.domain';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatChipsModule } from '@angular/material/chips';
 import { AddAssetsComponent } from './components/add-assets/add-assets.component';
 import { MatBottomSheet, MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
@@ -25,7 +26,8 @@ const MATERIAL_MODULES = [
   MatTabsModule,
   MatFormFieldModule,
   MatButtonModule,
-  MatBottomSheetModule
+  MatBottomSheetModule,
+  MatChipsModule
 ];
 
 @Component({
@@ -38,6 +40,9 @@ const MATERIAL_MODULES = [
 })
 export class SavingsComponent implements OnInit {
   public assets: IAsset[];
+  public filteredAssets: IAsset[];
+  public filters: Set<string> = new Set<string>();
+  public selectedFilter: string = 'All';
   public isPortfolioFrame: boolean = true;
 
   constructor(
@@ -45,13 +50,24 @@ export class SavingsComponent implements OnInit {
     private _bottomSheet: MatBottomSheet,
     private cdr: ChangeDetectorRef, 
   ) { }
-  
+
   public ngOnInit(): void {
     this.savingsService.getAll().subscribe(portfolio => {
       this.assets = portfolio;
+      this.filteredAssets = portfolio;
+      this.getAssetTypes();
     });
   }
 
+  public toggleChip(chipName: string): void {
+    this.selectedFilter = chipName;
+    this.filterAssets();
+  }
+
+  public isSelected(chipName: string): boolean {
+    return this.selectedFilter === chipName;
+  }
+  
   public onChangeFrame(frame: boolean): void {
     this.isPortfolioFrame = frame;
   }
@@ -61,7 +77,23 @@ export class SavingsComponent implements OnInit {
       switchMap(() => this.savingsService.getAll())
     ).subscribe(portfolio => {
       this.assets = portfolio;
+      this.getAssetTypes();
       this.cdr.detectChanges();
     });
+  }
+
+  private getAssetTypes(): void {
+    this.filters.add('All');
+    this.assets.forEach(asset => {
+      this.filters.add(asset.assetType);
+    });
+  }
+
+  private filterAssets(): void {
+    if (this.selectedFilter === 'All') {
+      this.filteredAssets = [...this.assets];
+    } else {
+      this.filteredAssets = this.assets.filter(asset => asset.assetType === this.selectedFilter);
+    }
   }
 }
