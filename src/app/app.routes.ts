baf0766e371b2c1e3bde447a0623cb2434a03808
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2024-01-05 14:07:55 +0200

    add savings routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,7 +12,7 @@ export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: AppRoutes.SPENDING},
   { path: AppRoutes.SPENDING, loadComponent: () => import('./pages/spending/spending.component').then(c => c.SpendingComponent) },
   { path: AppRoutes.CALENDAR, loadComponent: () => import('./pages/spending/components/calendar-spending/calendar-spending.component').then(c => c.CalendarSpendingComponent) },
-  { path: AppRoutes.SAVINGS, loadComponent: () => import('./pages/savings/savings.component').then(c => c.SavingsComponent) },
+  { path: AppRoutes.SAVINGS, loadChildren: () => import('./pages/savings/savings.routes').then(c => c.SAVINGS_ROUTES) },
   { path: AppRoutes.PROFILE, loadComponent: () => import('./pages/profile/profile.component').then(c => c.ProfileComponent) },
   { path: '**', loadComponent: () => import('./pages/not-found/not-found.component').then(c => c.NotFoundComponent) },
 ];
