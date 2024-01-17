70422b60ed715262fb9005df940e68bbd84481ed
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2024-01-17 13:15:57 +0200

    fix height bug in all pages

--- src/app/pages/goals/goals.component.ts
+++ src/app/pages/goals/goals.component.ts
@@ -2,11 +2,27 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { TotalBalanceComponent } from '../../core/UI/components/total-balance/total-balance.component';
 import { GoalCardComponent } from './components/goal-card/goal-card.component';
 import { IGoal } from '../../domain/goals.domain';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { ButtonToggleComponent } from '../../core/UI/components/button-toggle/button-toggle.component';
+import { MatButtonModule } from '@angular/material/button';
+
+
+const UI_COMPONENTS = [
+  TotalBalanceComponent,
+  GoalCardComponent,
+  ButtonToggleComponent
+];
+
+const MATERIAL_MODULES = [
+  MatIconModule,
+  MatButtonModule
+];
 
 @Component({
   selector: 'pgz-goals',
   standalone: true,
-  imports: [TotalBalanceComponent, GoalCardComponent],
+  imports: [...UI_COMPONENTS, ...MATERIAL_MODULES, RouterModule ],
   templateUrl: './goals.component.html',
   styleUrl: './goals.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -22,21 +38,21 @@ export class GoalsComponent implements OnInit {
         currentSum: 4000,
         finishSum: 40000,
         share: '20%',
-        status: 'active',
+        status: 'success',
       },
       {
         name: 'House',
         currentSum: 4000,
         finishSum: 40000,
         share: '20%',
-        status: 'active',
+        status: 'completed',
       },
       {
         name: 'House',
         currentSum: 4000,
         finishSum: 40000,
         share: '20%',
-        status: 'active',
+        status: 'progress',
       }
     ];
   }
