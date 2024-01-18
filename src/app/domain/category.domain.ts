a6aad74c25db52e0d005621df0aed3a917d5e5fd
froz-ferre <alexej.ferre@gmail.com>
2024-01-18 23:35:57 +0200

    change svg

--- src/app/domain/category.domain.ts
+++ src/app/domain/category.domain.ts
@@ -20,12 +20,12 @@ export class Category implements ICategory {
       new Category('Salary', 'money')
     ]),
     new Category('Spending', 'payments', '', [
-      new Category('Other', 'category', '', [
-        new Category('Test')
-      ]),
+      // new Category('Other', 'category', '', [
+      //   new Category('Test')
+      // ]),
       new Category('Clothes', 'custom_clothes'),
       new Category('Drink', 'custom_drink'),
-      new Category('Gift', 'custom_gift'),
+      new Category('Gift', 'redeem'),
       new Category('Car', 'custom_car'),
       new Category('Health', 'custom_health'),
       new Category('Food', 'custom_food'),
--- src/app/pages/spending/components/period-spending/period-spending.component.ts
+++ src/app/pages/spending/components/period-spending/period-spending.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { DonutComponent } from '../../../../core/UI/components/charts/donut/donut.component';
 import { ID3Value } from '../../../../domain/d3.domain';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
@@ -8,6 +8,13 @@ import { AddSpendingComponent } from '../add-spending/add-spending.component';
 import { switchMap } from 'rxjs';
 import { Category } from '../../../../domain/category.domain';
 import { IconComponent } from '../../../../core/UI/components/icon/icon.component';
+
+interface IViewCategory {
+  category: Category,
+  angle: number;
+}
+
+const FULL_CIRCLE_ANGLE: number = 360;
 
 
 const UI_COMPONENTS = [
@@ -24,17 +31,26 @@ const UI_COMPONENTS = [
   styleUrl: './period-spending.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PeriodSpendingComponent {
+export class PeriodSpendingComponent implements OnInit {
 
   @Input()
   public expends: ID3Value;
 
-  public categories: Category[] = Category.defaultList[1].children;
+  public viewCategories: IViewCategory[] = [];
+  private categories: Category[] = Category.defaultList[1].children;
 
   constructor(
     private _bottomSheet: MatBottomSheet,
     private expenseService: ExpenseService,
   ) { }
+
+  public ngOnInit(): void {
+    const angleStep: number = FULL_CIRCLE_ANGLE/this.categories.length;
+    this.viewCategories = this.categories.map((category: Category, index: number) => ({
+      category,
+      angle: angleStep * index,
+    }));
+  }
 
   public addSpending(category: Category): void {
     this._bottomSheet.open(AddSpendingComponent, {
