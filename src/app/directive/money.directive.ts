81dc45e0a942300d4b185d684b2351883196864b
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2024-05-06 23:23:04 +0300

    fix unhandled cases in money directive

--- src/app/directive/money.directive.ts
+++ src/app/directive/money.directive.ts
@@ -6,6 +6,23 @@ import { Directive, HostListener } from '@angular/core';
 })
 export class MoneyDirective {
 
+  @HostListener('keypress', ['$event'])
+  public onKeyPress(event: KeyboardEvent) {
+    const inputChar = String.fromCharCode(event.charCode);
+    const pattern = /^[0-9.]+$/;
+
+    const input = event.target as HTMLInputElement;
+    const inputValue = input.value;
+
+    if (!pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+
+    if (inputValue.includes('.') && inputChar === '.') {
+      event.preventDefault();
+    }
+  }
+
   @HostListener('input', ['$event'])
   public onInput(event: Event) {
     const input = event.target as HTMLInputElement;
