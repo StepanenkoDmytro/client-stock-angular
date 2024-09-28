aa5ee990284123f765fa76584a0a6b0c8c6074a3
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2024-09-28 23:53:03 +0300

    select with checkbox; unfinished

--- src/app/pages/spending/components/history-spending/history-spending.component.ts
+++ src/app/pages/spending/components/history-spending/history-spending.component.ts
@@ -1,9 +1,16 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import moment from 'moment';
 import { ICategorizedSpendings } from '../../../../domain/spending.domain';
 import { HistorySpendingCardComponent } from './history-spending-card/history-spending-card.component';
 import { Spending } from '../../model/Spending';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { Category } from '../../../../domain/category.domain';
+import { MatButtonModule } from '@angular/material/button';
 
 
 const UI_COMPONENTS = [
@@ -12,6 +19,12 @@ const UI_COMPONENTS = [
 
 const MATERIAL_MODULES = [
   MatIconModule,
+  FormsModule, 
+  MatFormFieldModule, 
+  MatSelectModule, 
+  MatCheckboxModule,
+  MatInputModule,
+  MatButtonModule
 ];
 
 @Component({
@@ -22,14 +35,61 @@ const MATERIAL_MODULES = [
   styleUrl: './history-spending.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HistorySpendingComponent {
+export class HistorySpendingComponent implements OnInit {
   
   @Input()
   public set spendings(value: Spending[]) {
     this.categoriesSpendings = this.categorizeSpendings([...value]);
   }
 
+  @Input()
+  public categories: Category[];
+
+  public selectedCategories: Category[] = [];
   public categoriesSpendings: ICategorizedSpendings;
+  public isAllCategoriesChecked: boolean = true; 
+  public selectedCategoriesValue: string[] = [];
+
+  private allCategoriesValue: string = 'All categories';
+
+  ngOnInit(): void {
+    this.selectAllCategories();
+  }
+
+  public toggleAllCategories(): void {
+    if (this.isAllCategoriesChecked) {
+      this.selectedCategories = [];
+    } else {
+      this.selectAllCategories();
+    }
+    this.isAllCategoriesChecked = !this.isAllCategoriesChecked;
+  }
+
+  private selectAllCategories(): void {
+    this.selectedCategories = [...this.categories];
+    this.isAllCategoriesChecked = true;
+  }
+
+  get displayValue(): string {
+    const isAllCategoriesSelected = this.selectedCategories.map(category => category.title).includes(this.allCategoriesValue)
+    if (isAllCategoriesSelected) {
+      return 'All selected';
+    }
+    return this.selectedCategoriesValue.join(', ');
+  }
+
+  public onCategoryChange(category: Category, checked: boolean): void {
+    if (checked) {
+      this.selectedCategories.push(category);
+    } else {
+      const index = this.selectedCategories.findIndex(c => c.id === category.id);
+      if (index > -1) {
+        this.selectedCategories.splice(index, 1);
+      }
+    }
+
+    this.isAllCategoriesChecked = this.selectedCategories.length === this.categories.length;
+  }
 
   private categorizeSpendings(spendings: Spending[]): ICategorizedSpendings {
     const today = moment().startOf('day');
