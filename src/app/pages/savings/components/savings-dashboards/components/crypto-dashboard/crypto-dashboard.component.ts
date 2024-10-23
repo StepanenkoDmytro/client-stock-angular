bcdcbb2e7cbd8a13b63fa87aedae8b88f6a18f29
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2024-10-23 21:05:03 +0300

    crypto card: first draft

--- src/app/pages/savings/components/savings-dashboards/components/crypto-dashboard/crypto-dashboard.component.ts
+++ src/app/pages/savings/components/savings-dashboards/components/crypto-dashboard/crypto-dashboard.component.ts
@@ -1,10 +1,20 @@
 import { AfterContentInit, ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { MoneyPipe } from '../../../../../../pipe/money.pipe';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+
+const MATERIAL = [
+  MatButtonModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+];
 
 @Component({
   selector: 'pgz-crypto-dashboard',
   standalone: true,
-  imports: [MoneyPipe],
+  imports: [MoneyPipe, ...MATERIAL],
   templateUrl: './crypto-dashboard.component.html',
   styleUrls: ['./crypto-dashboard.component.scss', '../dashboard.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
