620e38fdfeb358eff96f22fb2b35e9dc68793c54
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2025-01-04 21:41:24 +0200

    auth router fix

--- src/app/pages/auth/components/auth-container/auth-container.component.ts
+++ src/app/pages/auth/components/auth-container/auth-container.component.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { ArrowBackComponent } from '../../../../core/UI/components/arrow-back/arrow-back.component';
-import { AppRoutes } from '../../../../app.routes';
 
 @Component({
   selector: 'pgz-auth-container',
@@ -12,7 +11,6 @@ import { AppRoutes } from '../../../../app.routes';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AuthContainerComponent implements OnInit, OnDestroy {
-  public PROFILE_LINK = AppRoutes.PROFILE;
   public ngOnInit(): void {
     document.body.classList.add('hide-after');
   }
